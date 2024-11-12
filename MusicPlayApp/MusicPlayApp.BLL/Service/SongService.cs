c0f25d1e964e53134e866d901fa41149f8a24dbb
Project4Life <trungdnse183494@fpt.edu.vn>
2024-11-12 22:03:50 +0700

    repo and service

--- MusicPlayApp/MusicPlayApp.BLL/Service/SongService.cs
+++ MusicPlayApp/MusicPlayApp.BLL/Service/SongService.cs
@@ -1,25 +1,47 @@
 ﻿using MusicPlayApp.DLL.Model;
 using MusicPlayApp.DLL.Repository;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MusicPlayApp.BLL.Service
 {
     public class SongService
     {
-        private readonly SongRepo _songRepo = new();
+        private readonly SongRepo _songRepo;
 
+        // Inject SongRepo qua constructor
+        public SongService(SongRepo songRepo)
+        {
+            _songRepo = songRepo;
+        }
+
+        // Lấy tất cả bài hát
         public IEnumerable<Song> GetAllSongs()
         {
             return _songRepo.GetAllSongs();
         }
 
+        // Thêm bài hát mới
         public void AddSong(Song song)
         {
             _songRepo.AddSong(song);
+        }
+
+        // Cập nhật bài hát
+        public void UpdateSong(Song song)
+        {
+            _songRepo.UpdateSong(song);
+        }
+
+        // Xóa bài hát theo ID
+        public void DeleteSong(int id)
+        {
+            _songRepo.DeleteSong(id);
+        }
+
+        // Lấy bài hát theo ID
+        public Song GetSongById(int id)
+        {
+            return _songRepo.GetSongById(id);
         }
     }
 }
--- MusicPlayApp/MusicPlayApp.DLL/Repository/SongRepo.cs
+++ MusicPlayApp/MusicPlayApp.DLL/Repository/SongRepo.cs
@@ -1,27 +1,62 @@
 ﻿using MusicPlayApp.DLL.Model;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MusicPlayApp.DLL.Repository
 {
     public class SongRepo
     {
-        private MusicPlayListDbContext _context;
+        private readonly MusicPlayListDbContext _context;
 
+        // Inject DbContext thông qua constructor
+        public SongRepo(MusicPlayListDbContext context)
+        {
+            _context = context;
+        }
+
+        // Lấy tất cả bài hát
         public IEnumerable<Song> GetAllSongs()
         {
-            _context = new MusicPlayListDbContext();
             return _context.Songs.ToList();
         }
 
+        // Thêm bài hát mới
         public void AddSong(Song song)
         {
-            _context = new MusicPlayListDbContext();
             _context.Songs.Add(song);
             _context.SaveChanges();
         }
+
+        // Lấy bài hát theo ID
+        public Song GetSongById(int id)
+        {
+            return _context.Songs.Find(id);
+        }
+
+        // Xóa bài hát theo ID
+        public void DeleteSong(int id)
+        {
+            var song = _context.Songs.Find(id);
+            if (song != null)
+            {
+                _context.Songs.Remove(song);
+                _context.SaveChanges();
+            }
+        }
+
+        public void UpdateSong(Song song)
+        {
+            var existingSong = _context.Songs.Find(song.Id);
+            if (existingSong != null)
+            {
+                existingSong.Title = song.Title;
+                existingSong.Artist = song.Artist;
+                existingSong.Album = song.Album;
+                existingSong.Url = song.Url;
+
+                _context.SaveChanges();
+            }
+        }
+
     }
 }
